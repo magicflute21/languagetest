983b6cfd6b33e6af7ce4ad31166a12b2a2f8b704
Triin <triin.maesepp@websystems.ee>
2020-09-06 14:06:14 +0300

    small details

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,17 @@
 import '../scss/main.scss';
 import './dragdrop';
 
+// const express = require('express');
+// const app = express();
+
+// const port = process.env.port || 3000;
+// const hostname = '127.0.0.1';
+
+// app.get('/', (req, res) =>{
+//     res.status(200).send("Welcome to the homepage");
+// });
+
+
+// app.listen(port, hostname, () => {
+//     console.log(`Waazzaaapp, Server is running at http://${hostname}:${port}`);
+// });
